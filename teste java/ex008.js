729a66f855de10568ff04baecf3956f3b9c10521
Bruno <castro_bruno2006@hotmail.com>
2023-07-03 13:23:17 -0300

    testes java

---
+++ teste java/ex008.js
@@ -0,0 +1,13 @@
+
+var vel = 59.5
+console.log(`A velocidade do carrro é de ${vel}`)
+
+if(vel > 60)//condição simples
+{
+    console.log (`voce ultrapassou a velocidade permitida, multado!`)
+}
+else
+{
+console.log (`sua velocidade é a baixo do limite maximo, siga bem`)
+}
+console.log (`Sua velocidade é de ${vel} Dirija sempre usando o cinto de segurança`)
